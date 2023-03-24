af1f4e29d4b7c70f8a296ae9a94e120e94f91427
Francesco Antonetti Lamorgese Passeri <francescoantonetti1@gmail.com>
2023-03-24 17:32:16 +0100

    -

--- startSimulator.py
+++ startSimulator.py
@@ -7,28 +7,26 @@ if len(sys.argv)==4:
     gridBasePath  = sys.argv[1]
     gridName      = sys.argv[2]
     simulatorName = sys.argv[3]
+    gridBasePath = gridBasePath.rstrip("/")
 else:
     sys.exit("Wrong number of arguments")
 
 if not Path(gridBasePath).is_dir():
     sys.exit("Grid base path provided doesn't exist")
 
-print(gridBasePath)
-gridBasePath = gridBasePath.rstrip("/")
-print(gridBasePath)
-
 if not gridName.isalnum():
     sys.exit("Grid name is not alphanumeric only")
 
-print(gridName)
-
 if not simulatorName.isalnum():
     sys.exit("Simulator name is not alphanumeric only")
-
-print(simulatorName)
 
 if not Path(gridBasePath+'/simulators/'+simulatorName).is_dir():
     sys.exit("Simulator path doesn't exist")
 
 tmuxServer = libtmux.Server()
-tmuxServer.new_session(session_name=simulatorName,start_directory=gridBasePath+'/simulators/'+simulatorName+'/bin',window_name=simulatorName.capitalize(),window_command='opensim.sh',attach=False)
+
+try:
+    tmuxServer.new_session(session_name=simulatorName,start_directory=gridBasePath+'/simulators/'+simulatorName+'/bin',window_name=simulatorName.capitalize(),window_command='opensim.sh',attach=False)
+except Exception as e:
+    print(str(e))
+    sys.exit(1)
