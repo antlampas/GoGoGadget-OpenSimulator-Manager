4cc5b2f7b9f405ae03a4c9b9f61a8179fc0827c2
Francesco Antonetti Lamorgese Passeri <francescoantonetti1@gmail.com>
2023-05-08 22:55:25 +0200

    -

--- startSimulator.py
+++ startSimulator.py
@@ -2,6 +2,11 @@
 #Date: 2023-03-15
 #This work is licensed under the Creative Commons Attribution 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
+"""Start Simulator
+
+
+"""
+
 from pathlib import Path
 from sys import exit,argv
 import libtmux 
