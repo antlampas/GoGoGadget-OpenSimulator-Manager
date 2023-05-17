05b4c62a40dc6b31493d4eb7ddbd92b2942e285c
Francesco Antonetti Lamorgese Passeri <francescoantonetti1@gmail.com>
2023-05-17 14:00:32 +0200

    -

---
+++ createSimulator.py
@@ -0,0 +1,14 @@
+#Author: antlampas
+#Date: 2023-05-17
+#This work is licensed under the Creative Commons Attribution 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+import pathlib,dbManager
+
+class createSimulator:
+    """Create Simulator
+
+    Makes and sets all the paths related to the simulator and puts the
+    opensimulator instance in the main simulator path.
+    """ 
+    def __init__(self,gridName="",simulatorName="",basePath="",baseConfPath="",baseLogPath=""):
+        pass
